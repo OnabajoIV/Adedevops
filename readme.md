348ac2c5a5fb0364f306f7a8000b0ea712b19322
root <onabajo.nathaniel@gmail.com>
2022-04-09 02:20:15 +0000

    First trial

---
+++ readme.md
@@ -0,0 +1,7 @@
+This is a read me file
+
+The script in the repo is to create users and do some other tasks.
+You can run the script using bash script.sh or "./script.sh"
+
+Thank you!
+
---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "welcome to scripting lifestyle"
+echo
+sleep 3
+echo
+echo "if you like i can create a user with a directory for you"
+sleep 2
+echo
+echo "would you like me to go ahead and create a personal user for you?"
