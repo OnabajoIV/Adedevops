2d5f6217291a0d4eda04c96784289e1dc4a5a4df
root <onabajo.nathaniel@gmail.com>
2022-04-15 01:26:25 +0000

    New changes to script.sh

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ echo "if you like i can create a user with a directory for you"
 sleep 2
 echo
 echo "would you like me to go ahead and create a personal user for you?"
-read answer
+#read answer
 #write an if statement, if the answer is yes continue, if it's no quit
 echo
 echo "Thanks for using my services"
@@ -20,9 +20,9 @@ echo "3. create a file"
 echo "4. create a folder"
 echo "5. create a new webserver"
 #write a case statement that will perform any action the user picks
-read option 
+#read option 
 sleep 2
 echo "you have selected option $option"
 echo 
 echo "gpt ya, let me work on this"
-
+echo "done"
